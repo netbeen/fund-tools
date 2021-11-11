f7f07095b33388dcfca76a022d16fca6798e35e9
Yang Yang <yangyang.jeff@bytedance.com>
2021-11-11 12:27:13 +0800

    chore: remove useless test

---
+++ test/calcReturn.test.js
@@ -0,0 +1,22 @@
+import dayjs, { calcReturn } from '../src'
+import timezone from '../src/plugin/timezone'
+import utc from '../src/plugin/utc'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+const mockMarketPrice = [
+  { date: new Date('2021-01-01'), price: 1 },
+  { date: new Date('2021-01-02'), price: 2 },
+  { date: new Date('2021-01-03'), price: 2 },
+  { date: new Date('2021-01-04'), price: 3 },
+  { date: new Date('2021-01-05'), price: 4 },
+  { date: new Date('2021-01-06'), price: 5 },
+  { date: new Date('2021-01-07'), price: 1 },
+  { date: new Date('2021-01-08'), price: 1 }
+]
+
+it('calcReturn Test', () => {
+  const result = calcReturn({ marketPrices: mockMarketPrice, operations: [] })
+  console.log('result', result)
+})
