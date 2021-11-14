cb25b10c4fb2dfdf70a8259ae3da726657fc50da
Yang Yang <yangyang.jeff@bytedance.com>
2021-11-14 15:40:53 +0800

    chore: update d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
-export function calcReturn(arr: any[]): void;
-export function fetchUnitPriceByIdentifier(arr: any[]): void;
+import * as dayjs from 'dayjs'
+export function calcReturn(identifier: string): Promise<{date: dayjs.Dayjs, price: number}>;
+export function fetchUnitPriceByIdentifier(identifier: string): Promise<Array<{date: dayjs.Dayjs, price: number}>>;
 export function fetchSplitByIdentifier(arr: any[]): void;
 export function fetchAccumulatedPriceByIdentifier(arr: any[]): void;
 export function fetchDividendByIdentifier(arr: any[]): void;
