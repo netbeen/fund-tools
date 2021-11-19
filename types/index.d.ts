f2026d62fa92a8ebd577896cca9ecb220f8ae45f
Yang Yang <yangyang.jeff@bytedance.com>
2021-11-19 20:03:27 +0800

    feat: update type.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -27,4 +27,4 @@ export function fetchUnitPriceByIdentifier(identifier: string): Promise<Array<{d
 export function fetchAccumulatedPriceByIdentifier(identifier: string): Promise<Array<{date: dayjs.Dayjs, price: number}>>;
 export function fetchSplitByIdentifier(identifier: string): Promise<Array<{date: dayjs.Dayjs, splitRatio: number}>>;
 export function fetchDividendByIdentifier(identifier: string): Promise<Array<{date: dayjs.Dayjs, dividend: number}>>;
-export function fetchBasicInfoByIdentifier(identifier: string): Promise<Array<{identifier: string, name: string, type: string}>>;
+export function fetchBasicInfoByIdentifier(identifier: string): Promise<{identifier: string, name: string, type: string}>;
