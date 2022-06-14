bb9a3c80a85ad25d2efae4a22fbf8753ee85adca
Yang Yang <yangyang.jeff@bytedance.com>
2022-06-14 08:51:44 +0800

    test: add ut coverage

--- test/crawler.test.js
+++ test/crawler.test.js
@@ -5,7 +5,8 @@ test('Fetch some example price data', async () => {
     fetchUnitPriceByIdentifier('519671'),
     fetchAccumulatedPriceByIdentifier('519671'),
     fetchDividendByIdentifier('519671'),
-    fetchSplitByIdentifier('512010')])
+    fetchSplitByIdentifier('512010')
+  ])
   expect(unitResult.length).toBeGreaterThan(0)
   expect(accumulatedResult.length).toBeGreaterThan(0)
   expect(dividendsResult.length).toBeGreaterThan(0)
@@ -19,4 +20,9 @@ test('Fetch basic info', async () => {
   ])
   expect(validResult).toEqual({ identifier: '160119', name: '南方中证500ETF联接(LOF)A', type: '股票型' })
   expect(invalidResult).toBeNull()
+})
+
+test('Fetch with error', async () => {
+  await expect(fetchUnitPriceByIdentifier('160119xxx')).rejects.toThrow()
+  await expect(fetchAccumulatedPriceByIdentifier('160119xxx')).rejects.toThrow()
 })
