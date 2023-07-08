10a1819557a2883f8ec0f1bee8eb66caca648f62
Yang Yang <yangyang.jeff@bytedance.com>
2023-07-08 19:09:03 +0800

    chore: add calcVolume function

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   verbose: true,
   roots: ['src/tests'],
+  moduleNameMapper: {
+    '@/return': 'src/return',
+    '@/constants': 'src/constants',
+  },
   testRegex: 'tests/(.*?/)?.*test.ts$',
   testEnvironment: 'node',
   coverageDirectory: './coverage/',
